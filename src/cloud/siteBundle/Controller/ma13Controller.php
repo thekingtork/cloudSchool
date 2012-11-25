0276a4441cc2949ced09fb26ea074ca0fff4706b
Amado J. Ramos Arboleda <amado0529@gmail.com>
2012-11-25 17:58:01 -0500

    MA13221

--- src/cloud/siteBundle/Controller/ma13Controller.php
+++ src/cloud/siteBundle/Controller/ma13Controller.php
@@ -8,10 +8,12 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use cloud\siteBundle\Entity\Periodo;
+use cloud\siteBundle\Entity\Grado;
 use cloud\siteBundle\Entity\NivelesAcademicos;
 
 use cloud\siteBundle\Form\PeriodoType;
 use cloud\siteBundle\Form\NivelesAcademicosType;
+use cloud\siteBundle\Form\GradoType;
 
 /**
  * Periodo controller.
@@ -95,12 +97,93 @@ class ma13Controller extends Controller
      public function ma1322Action(){
         $em=$this->getDoctrine()->getEntityManager();
         $niveles=$em->getRepository('cloudBundle:NivelesAcademicos')->findAll();
+
         if(!$niveles)
             return $this->render('cloudBundle:Admin:ma1322.html.twig', array('mje'=>'No tiene tiene niveles academicos asociadas'));
         return $this->render('cloudBundle:Admin:ma1322.html.twig', array('niveles'=>$niveles));
      }
 
+      /** 
+      * @Route("/niveles-grados/grados/{id}/crear", name="ma13221")
+      */
+     public function ma13221Action($id){
+        $request=$this->getRequest();
+        $em=$this->getDoctrine()->getEntityManager();
+        $nivel = $em->getRepository('cloudBundle:NivelesAcademicos')->find($id);
 
+        if($request->getMethod()=='POST')
+        {
+            $numero=$request->request->get('numsede');
+            for ($i=1; $i <= $numero; $i++) { 
+                $entity  = new Grado();
+                $entity->setName("Grado");
+                $entity->setNivelId($nivel);
+                $em->persist($entity);
+                $em->flush();
+            }
+            $grados = $em->getRepository('cloudBundle:Grado')->findBy(array('nivel_id'=>$id));
+
+            return $this->render('cloudBundle:Admin:ma13221.html.twig', array('grados'=>$grados));
+        }
+        
+        $grados=$em->getRepository('cloudBundle:Grado')->findBy(array( 'nivel_id'=>$id));
+        if(!$grados)
+            return $this->render('cloudBundle:Admin:ma13221.html.twig', array('mje'=>'No tiene tiene grados academicos asociadas'));
+        return $this->render('cloudBundle:Admin:ma13221.html.twig', array('grados'=>$grados));
+     }
+
+      /**
+     *
+     * @Route("/niveles-grados/grados/{id}/edit", name="ma13222")
+     */
+    public function ma13222Action($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('cloudBundle:Grado')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Grado entity.');
+        }
+
+        $editForm = $this->createForm(new GradoType(), $entity);
+
+        return $this->render('cloudBundle:Admin:ma13222.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+        ));
+    }
+
+     /**
+     *
+     * @Route("/niveles-grados/grados/{id}/update", name="ma13223")
+     * @Method("POST")
+     */
+    public function ma13223Action(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('cloudBundle:Grado')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Grado entity.');
+        }
+
+        
+        $editForm = $this->createForm(new GradoType(), $entity);
+        $editForm->bind($request);
+
+        if ($editForm->isValid()) {
+            $em->persist($entity);
+            $em->flush();
+
+            $nivel = $entity->getNivelId();
+            return $this->redirect($this->generateUrl('ma13221',array('id'=> $nivel->getId() )) );
+        }
+
+        $entity = $em->getRepository('cloudBundle:Grado')->findAll();
+        return $this->render('cloudBundle:Admin:ma13222.html.twig', array('grados'=>$entity));
+    }
 
 
 
@@ -186,6 +269,7 @@ class ma13Controller extends Controller
             return $this->redirect($this->generateUrl('ma1321') );
         }
 
+        $niveles = $em->getRepository('cloudBundle:NivelesAcademicos')->findAll();
         return $this->render('cloudBundle:Admin:ma1321.html.twig', array('niveles'=>$niveles));
     }
 
---
+++ src/cloud/siteBundle/Form/GradoType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace cloud\siteBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class GradoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'cloud\siteBundle\Entity\Grado'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'form_Grado';
+    }
+}
