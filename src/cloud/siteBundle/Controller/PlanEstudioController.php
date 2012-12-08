e0bfcc235dae6c2ce93c3543e11982bab6f6064a
Oscar Bertel <osber00@gmail.com>
2012-12-08 14:49:53 -0500

    Controlador plan de estudio

---
+++ src/cloud/siteBundle/Controller/PlanEstudioController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace cloud\siteBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+/**
+ * Periodo controller.
+ *
+ * @Route("/admin/planestudio")
+ */
+class PlanEstudioController extends Controller
+{
+    
+
+}
