33e6f940349e894941811aba5e9c01400b5352eb
Amado J. Ramos Arboleda <amado0529@gmail.com>
2012-12-18 22:21:58 -0500

    ma3

--- src/cloud/siteBundle/Controller/MA3/MA3Controller.php
+++ src/cloud/siteBundle/Controller/MA3/MA3Controller.php
@@ -8,6 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use cloud\siteBundle\Form\PlanEstudioType;
+use cloud\siteBundle\Form\ActividadType;
 use cloud\siteBundle\Entity\PlanEstudio;
 use cloud\siteBundle\Entity\Actividad;
 
@@ -18,7 +19,19 @@ use cloud\siteBundle\Entity\Actividad;
  */
 class MA3Controller extends Controller{
 
-    
+     /**
+     * 
+     * @Route("/", name="ma3")
+     */
+    public function ma3Action()
+    {
+        $em=$this->getDoctrine()->getEntityManager();
+        $entity = $em->getRepository('cloudBundle:PlanEstudio')->findAll();
+        if(!$entity)
+            return $this->redirect($this->generateUrl('ma32',array('id'=>$plan->getId())));
+        return $this->render('cloudBundle:Admin:MA3/ma3.html.twig',array('entities'=>$entity));
+
+    }
     
     /**
      * 
@@ -69,13 +82,13 @@ class MA3Controller extends Controller{
                 $em->persist($actividades);
                 $em->flush();
             }
-         return $this->redirect($this->generateUrl('ma32',array('id'=>$plan->getId())));
+         return $this->redirect($this->generateUrl('ma312',array('id'=>$plan->getId())));
     }
 
 
     /**
      * 
-     * @Route("/", name="ma3")
+     * 
      * @Route("/addplanes", name="ma311")
      */
     public function ma311Action(){
@@ -87,13 +100,26 @@ class MA3Controller extends Controller{
 
      /**
      * 
-     * @Route("/{id}/actividades/", name="ma32")
+     * @Route("/{id}/actividades/", name="ma312")
      * 
      */
-    public function ma32Action(){
-        $request= $this->getRequest();
-        $plan= new PlanEstudio();
-        $form=$this->createForm(new PlanEstudioType(),$plan);
-        return $this->render('cloudBundle:Admin:MA3/ma311.html.twig',array('form'=>$form->createView()));
+    public function ma312Action($id){
+        $em=$this->getDoctrine()->getEntityManager();
+        $entity = $em->getRepository('cloudBundle:Actividad')->findBy(array('plan_id'=> $id));
+        return $this->render('cloudBundle:Admin:MA3/ma312.html.twig',array('entities'=>$entity,'id'=>$id) );
+    }
+
+     /**
+     * 
+     * @Route("/{id}/actividades/{id2}/edit", name="ma313")
+     * 
+     */
+    public function ma313Action($id,$id2){
+        $em=$this->getDoctrine()->getEntityManager();
+        $entity = $em->getRepository('cloudBundle:Actividad')->find($id2);
+        $entity_plan = $em->getRepository('cloudBundle:PlanEstudio')->find($id);
+        $form=$this->createForm(new ActividadType(),$entity);
+
+        return $this->render('cloudBundle:Admin:MA3/ma313.html.twig',array('entities'=>$entity,'id'=>$id, 'edit_form'=>$form->createView(),'entity_plan'=>$entity_plan) );
     }
 }
